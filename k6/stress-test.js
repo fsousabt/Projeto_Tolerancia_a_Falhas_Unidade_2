078543ba316d0109faaf8decf19d6b3606e6501a
Ivison Filho <ivisoncaufilho@gmail.com>
2025-12-08 12:30:08 -0300

    Atualizando stress test

--- k6/stress-test.js
+++ k6/stress-test.js
@@ -4,27 +4,29 @@ import faker from 'k6/x/faker';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 10 },
-    { duration: '1m', target: 10 },
-    { duration: '30s', target: 0 },
+    { duration: '30s', target: 50 },   // ramp-up inicial
+    { duration: '30s', target: 200 },  // comeca o stress
+    { duration: '60s', target: 300 },  // pico
+    { duration: '30s', target: 300 },  // mantem
+    { duration: '30s', target: 0 },    // ramp-down
   ]
+};
 
-}
 
 export default function() {
-  // const payload = JSON.stringify({
-  //   "flight": faker.strings.digitN(8),
-  //   "day": faker.time.dateRange("2026-01-01", "2026-12-31", "yyyy-MM-dd"),
-  //   "user": faker.person.firstName(),
-  //   "ft": true
-  // });
-
   const payload = JSON.stringify({
-    "flight": "11111111",
-    "day": "2026-01-01",
+    "flight": faker.strings.digitN(8),
+    "day": faker.time.dateRange("2026-01-01", "2026-12-31", "yyyy-MM-dd"),
     "user": faker.person.firstName(),
     "ft": false
   });
+
+  // const payload = JSON.stringify({
+  //   "flight": "11111111",
+  //   "day": "2026-01-01",
+  //   "user": faker.person.firstName(),
+  //   "ft": false
+  // });
 
   const params = {
     headers: {
@@ -34,6 +36,6 @@ export default function() {
 
   const response = http.post('http://127.0.0.1:8080/buyTicket', payload, params)
   check(response, { 'status is 200': (r) => r.status === 200 || r.status === 504 });
-  sleep(0.5);
+  sleep(0.2);
 
 }
